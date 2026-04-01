92d3e75a1bb669f9243f95a1de455977aaec880d
Tod <tpoirier@nmu.edu>
2026-04-01 18:01:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.RECIPE_APP_CONFIG = {
   supabaseUrl: 'https://wntakzfoprthwggkidyq.supabase.co',
-  supabaseAnonKey: 'sb_publishable_gWu_EQ1J3s0iNjeDeINJwQ_xKy8QgAJ',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndudGFremZvcHJ0aHdnZ2tpZHlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2MDc1OTAsImV4cCI6MjA4OTE4MzU5MH0.DARhIw0z5bXxEVTinx-6SIPQg5vnFJmVooJN4_Gqya8',
   storageBucket: 'foodie_recipe_assets'
 };
