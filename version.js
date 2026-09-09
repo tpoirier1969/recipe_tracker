0e9e5da7a333094c0041d035e4743348622781de
Tod <tpoirier@nmu.edu>
2026-09-09 09:25:52 -0400

    Prevent startup icon flash: update version.js

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '0.18.0';
+  const VERSION = '0.18.1';
   const DISPLAY_VERSION = `v${VERSION}`;
   const scriptUrl = new URL(
     document.currentScript?.src || window.location.href,
